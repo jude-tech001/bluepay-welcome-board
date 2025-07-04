f7e73adf5b71848cc1f43272f89836e5d0ebdb8d
Jude-the-wizard☠️ <sjude9928@gmail.com>
2025-07-05 00:55:18 +0100

    Update AccountValidationPage.tsx

--- src/components/AccountValidationPage.tsx
+++ src/components/AccountValidationPage.tsx
@@ -303,8 +303,8 @@ const AccountValidationPage: React.FC<AccountValidationPageProps> = ({ onBack, u
                 <div className="flex justify-between items-center">
                   <span className="text-blue-100">Account Name:</span>
                   <div className="flex items-center gap-2">
-                    <span className="font-medium">JOEL SAMUEL</span>
-                    <button onClick={() => copyToClipboard('bluepay-jude Samuel')} className="text-blue-200 hover:text-white">
+                    <span className="font-medium">CHUKWUEMEKA.A.JAMES</span>
+                    <button onClick={() => copyToClipboard('Chukwuemeka amadi James')} className="text-blue-200 hover:text-white">
                       <Copy size={16} />
                     </button>
                   </div>
